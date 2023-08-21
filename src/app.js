a669248c93fc3838ffbe1385be14f82b576b3252
Gırık <yunus.yk2000@gmail.com>
2023-08-21 15:28:45 +0300

    Js one

---
+++ src/app.js
@@ -0,0 +1,25 @@
+//camelCasing 
+
+console.log("Nörün dayı oğlu");
+
+let dolarDun = 9.20
+let dolarBugun = 9.30
+
+console.log(dolarDun)
+
+const euroDun = 11.2
+// euroDun = 11 // bu işlem hatalıdır.
+
+console.log(euroDun)
+
+// array 
+let konutKredileri = ["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Özel Konut Kredisi"]
+
+
+for(let i = 0; i < konutKredileri.length; i++){
+    console.log("<li>"+ konutKredileri[i] +"</li>")
+}
+
+console.log(konutKredileri)
+
+
