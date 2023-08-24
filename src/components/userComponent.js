037a23bbeaa275a320fec5c636e335482e578d78
Gırık <yunus.yk2000@gmail.com>
2023-08-24 21:06:20 +0300

    day 4

--- src/components/userComponent.js
+++ src/components/userComponent.js
@@ -1,4 +1,5 @@
 import { BaseLogger, ElasticLogger, MongoLogger } from "../crossCuttingConcerns/logging/logger.js"
+import Customer from "../models/customer.js"
 import User from "../models/user.js"
 import UserService from "../services/userService.js"
 
@@ -7,18 +8,18 @@ console.log("User component yüklendi")
 let logger1 = new MongoLogger()
 let userService = new UserService(logger1)
 
-let user1 = new User(1,"Yunus","Koçak","Ankara")
-let user2 = new User(2,"Murat","Kayıhan","Antalya")
+// let user1 = new User(1,"Yunus","Koçak","Ankara")
+// let user2 = new User(2,"Murat","Kayıhan","Antalya")
 
-userService.add(user1)
-userService.add(user2)
+// userService.add(user1)
+// userService.add(user2)
 
-console.log(userService.list())
-console.log(userService.getById(2))
+// console.log(userService.list())
+// console.log(userService.getById(2))
 
 
-userService.getById(1)
-userService.list()
+// userService.getById(1)
+// userService.list()
 
 console.log("-------------------------------")
 
@@ -26,3 +27,14 @@ let customer = {id:1,firstName:"Yunus"}
 
 // prototyping
 customer.lastName = "Koçak"
+
+console.log("-------------------------------")
+
+userService.load();
+let customerToAdd = new Customer (6,"Murat","Kayıhan","Antalya","30")
+customerToAdd.type = "customer"
+userService.add(customerToAdd)
+console.log(userService.customers)
+console.log(userService.employees)
+console.log(userService.errors)
+console.log(userService.getCustomersSorted())
---
+++ src/data/users.js
@@ -0,0 +1,58 @@
+
+// id, firstName, lastName, city, age, creditCardNumber
+export const users = [
+    {
+        id:1,
+        firstName:"Yunus",
+        lastName:"Koçak",
+        city:"Konya",
+        age:"24",
+        creditCardNumber:"123456",
+        type:"customer"
+    },
+    {
+        id:2,
+        firstName:"Ahmet",
+        lastName:"Altın",
+        city:"İzmir",
+        age:"32",
+        creditCardNumber:"654321",
+        type:"customer"
+    },
+    {
+        id:6,
+        firstName:"Mustafa",
+        lastName:"Ak",
+        city:"İzmir",
+        age:"27",
+        creditCardNumber:"456654",
+        type:"customer"
+    },
+    {
+        id:3,
+        firstName:"Ali",
+        lastName:"Kuzu",
+        city:"İstanbul",
+        age:"20",
+        salary: 8500,
+        type:"employee"
+    },
+    {
+        id:4,
+        firstName:"Ceyda",
+        lastName:"Kaz",
+        city:"Ankara",
+        age:"26",
+        salary: 8500,
+        type:"employee"
+    },
+    {
+        id:5,
+        firstName:"Ayhan",
+        lastName:"Aydın",
+        city:"Muş",
+        age:"20",
+        salary: 10000,
+        type:"employee"
+    },
+]
